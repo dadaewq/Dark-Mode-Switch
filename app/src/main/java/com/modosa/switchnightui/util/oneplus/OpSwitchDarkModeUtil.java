72b1258129d429c32772fa18d162c26642829a60
dadaewqq <dadaewqq@gmail.com>
2020-05-04 17:42:18 +0800

    一加同步切换系统主题前先检查Root权限

--- app/src/main/java/com/modosa/switchnightui/util/oneplus/OpSwitchDarkModeUtil.java
+++ app/src/main/java/com/modosa/switchnightui/util/oneplus/OpSwitchDarkModeUtil.java
@@ -13,31 +13,34 @@ import static com.modosa.switchnightui.util.OpUtil.BLANK;
 public class OpSwitchDarkModeUtil {
 
     public static void setDarkMode(Context context, boolean enable) {
-        String cmd;
-        if (enable) {
-            OPThemeUtils.enableDarkThemes(context);
-            cmd = OpSwitchForceDarkUtil.CMD_SETPROP_THEME + 2;
-        } else {
-            int getOemBlackMode = Settings.System.getInt(context.getContentResolver(), OPThemeUtils.KEY_DARK_MODE_ACTION, 0);
 
-            if (getOemBlackMode == 1) {
-                getOemBlackMode = 2;
-            }
-
-            cmd = OPThemeUtils.CMD_SETTINGS_PUT_SYSTEM + OPThemeUtils.KEY_ORIGIN_DARK_MODE_ACTION + BLANK + getOemBlackMode;
-            ShellUtil.execWithRoot(cmd);
-
-            int oneplusTheme = Settings.System.getInt(context.getContentResolver(), OPThemeUtils.KEY_ORIGIN_DARK_MODE_ACTION, 0);
-
-            if (oneplusTheme == 2) {
-                OPThemeUtils.enableColorfulThemes(context);
+        if ("0".equals(ShellUtil.execWithRoot("exit")[3])) {
+            String cmd;
+            if (enable) {
+                OPThemeUtils.enableDarkThemes(context);
+                cmd = OpSwitchForceDarkUtil.CMD_SETPROP_THEME + 2;
             } else {
-                OPThemeUtils.enableLightThemes(context);
-            }
+                int getOemBlackMode = Settings.System.getInt(context.getContentResolver(), OPThemeUtils.KEY_DARK_MODE_ACTION, 0);
 
-            cmd = OpSwitchForceDarkUtil.CMD_SETPROP_THEME + 1;
+                if (getOemBlackMode == 1) {
+                    getOemBlackMode = 2;
+                }
+
+                cmd = OPThemeUtils.CMD_SETTINGS_PUT_SYSTEM + OPThemeUtils.KEY_ORIGIN_DARK_MODE_ACTION + BLANK + getOemBlackMode;
+                ShellUtil.execWithRoot(cmd);
+
+                int oneplusTheme = Settings.System.getInt(context.getContentResolver(), OPThemeUtils.KEY_ORIGIN_DARK_MODE_ACTION, 0);
+
+                if (oneplusTheme == 2) {
+                    OPThemeUtils.enableColorfulThemes(context);
+                } else {
+                    OPThemeUtils.enableLightThemes(context);
+                }
+
+                cmd = OpSwitchForceDarkUtil.CMD_SETPROP_THEME + 1;
+            }
+            ShellUtil.execWithRoot(cmd);
         }
-        ShellUtil.execWithRoot(cmd);
     }
 
 }
